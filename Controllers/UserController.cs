dc4800fafb0be240858dd1c9f0176cdefcaf2c2f
desmondlch <desmondleongch@gmail.com>
2024-12-01 17:53:06 +0800

    first commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using azure_toolkit.Models;
+
+namespace azure_toolkit.Controllers
+{
+    [Route("api/User")]
+    public class UserController : Controller
+    {
+        static readonly List<UserModel> _users = new List<UserModel>
+        {
+            new UserModel { Name="admin", Login="admin", Password="admin" }
+        };
+
+        [HttpPost]
+        public IActionResult Auth([FromBody] UserModel model)
+        {
+            return Json(
+                _users.FirstOrDefault(
+                    u =>
+                        u.Login.Equals(model.Login) &&
+                        u.Password.Equals(model.Password)));
+        }
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,13 @@
+﻿namespace azure_toolkit.Models
+{
+    public class UserModel
+    {
+        public string Name { get; set; }
+
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+
+        public string RefreshToken { get; set; }
+    }
+}
